2910ecb09f0a908ffb80482fadaf2916e6621ad2
Felipe Breda <felipebreda92@gmail.com>
2019-01-20 16:10:23 -0200

    Create first controller

---
+++ Api.RestFull/Api.RestFull/Controllers/CalculatorController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.RestFull.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet("{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid Imput");
+        }
+
+        private bool IsNumeric(string valor)
+        {
+            int val;
+            return int.TryParse(valor, out val);
+        }
+
+        private decimal ConvertToDecimal(string valor)
+        {
+            return decimal.Parse(valor);
+        }
+    }
+}
