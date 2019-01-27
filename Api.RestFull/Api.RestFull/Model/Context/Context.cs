779dc62efb9f0cce09cc015d5394d393b837f2aa
felipebreda92 <felipebreda92@gmail.com>
2019-01-27 01:20:11 -0200

    Connection Cinfiguration finish

---
+++ Api.RestFull/Api.RestFull/Model/Context/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.RestFull.Model.Context
+{
+    public class Context : DbContext
+    {
+        public Context() { }
+
+        public Context(DbContextOptions<Context> options) : base(options) { }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+}
