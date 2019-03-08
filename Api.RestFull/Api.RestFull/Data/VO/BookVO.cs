075c9520bb6e077024edb1b7d9a6eb0a43e71eff
felipebreda92 <felipebreda92@gmail.com>
2019-03-08 08:50:52 -0300

    DataContract

--- Api.RestFull/Api.RestFull/Data/VO/BookVO.cs
+++ Api.RestFull/Api.RestFull/Data/VO/BookVO.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Threading.Tasks;
 
 namespace Api.RestFull.Data.VO
 {
+    //[DataContract]
     public class BookVO
     {
+        //[DataMember(Order = 1, Name = "Codigo")]
         public long Id { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
